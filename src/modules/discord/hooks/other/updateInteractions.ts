881a81c2e5b02f83a97817e0ad21d85545070d2c
GaryCraft <garycraft@our-space.xyz>
2025-05-29 18:35:08 -0600

    Fix updateinteractions

--- src/modules/discord/hooks/other/updateInteractions.ts
+++ src/modules/discord/hooks/other/updateInteractions.ts
@@ -1,24 +1,33 @@
-import { ApplicationCommandDataResolvable, Guild } from "discord.js";
+import { ApplicationCommandDataResolvable, ApplicationCommandType, Guild } from "discord.js";
 import ExtendedClient, { DSInteraction } from "../../extendedclient";
 import { error, info } from "@src/engine/utils/Logger";
-interface CleanInteraction {
-	name: string;
-	description?: string;
-	registerTo?: DSInteraction["registerTo"];
+
+function typeFromType(type: DSInteraction["type"]): ApplicationCommandType {
+	if (type == "chat") return ApplicationCommandType.ChatInput
+	if (type == "message") return ApplicationCommandType.Message
+	if (type == "user") return ApplicationCommandType.User
+	return ApplicationCommandType.PrimaryEntryPoint
 }
 
 export default async function updateInteractions(client: ExtendedClient, params: { guild?: Guild } | undefined) {
 	const guild = params?.guild;
 	if (!guild) {
-		const arr: ApplicationCommandDataResolvable[] = [];
+		const arr = [];
 		for await (const [, interaction] of client.interactions.entries()) {
-			const cleanInt: CleanInteraction = {
-				name: interaction.name,
-				registerTo: interaction.registerTo,
-			}
-			cleanInt.description = interaction.type == "chat" ? undefined : interaction.description;
+			const cleanInt: Partial<ApplicationCommandDataResolvable> =
+				interaction.type == "chat" ?
+					{
+						name: interaction.name,
+						type: typeFromType(interaction.type),
+						description: interaction.description
+					} : {
+						name: interaction.name,
+						type: typeFromType(interaction.type),
+
+					}
+			if (interaction.registerTo == "app") arr.push(cleanInt);
 		}
-		client.application?.commands.set(arr).catch((e) => {
+		client.application?.commands.set(arr as ApplicationCommandDataResolvable[]).catch((e) => {
 			error("Error while updating App Interactions: ", e);
 		});
 		info("Updated App Interactions");
@@ -26,12 +35,18 @@ export default async function updateInteractions(client: ExtendedClient, params:
 	}
 	const arr = [];
 	for await (const [, interaction] of client.interactions.entries()) {
-		const cleanInt: CleanInteraction = {
-			name: interaction.name,
-			registerTo: interaction.registerTo,
-		}
-		cleanInt.description = interaction.type == "chat" ? undefined : interaction.description;
-		if (cleanInt.registerTo == "guild") arr.push(cleanInt);
+		const cleanInt: Partial<ApplicationCommandDataResolvable> =
+			interaction.type == "chat" ?
+				{
+					name: interaction.name,
+					type: typeFromType(interaction.type),
+					description: interaction.description
+				} : {
+					name: interaction.name,
+					type: typeFromType(interaction.type),
+
+				}
+		if (interaction.registerTo == "guild") arr.push(cleanInt);
 	}
 	await guild.commands.set(arr as ApplicationCommandDataResolvable[]).catch((e) => {
 		error("Found Guild but couldn't Update Interactions", e);
